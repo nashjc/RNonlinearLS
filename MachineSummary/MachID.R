733ccfb29366d677d5b8ef4646c5b805d4130054
Arkajyoti Bhattacharjee <arkastat98@gmail.com>
2022-06-13 18:23:46 +0530

    changed tsstr format as in the main doc

--- MachineSummary/MachID.R
+++ MachineSummary/MachID.R
@@ -1,5 +1,5 @@
 sy <- Sys.info()
-tsstr <- format(Sys.time(), "%Y%m%d%H%M")
+tsstr <- format(Sys.time(), "%Y/%m/%d|%H:%M")
 cpu <- benchmarkme::get_cpu()
 ram <- benchmarkme::get_ram()
 machid <- paste(sy["nodename"],":",sy["user"],"-",sy["sysname"],"-",sy["release"],
