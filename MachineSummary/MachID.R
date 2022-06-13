9eb49547203b69d4acbca0f6797ad84e65b9f892
Arkajyoti Bhattacharjee <arkastat98@gmail.com>
2022-06-13 17:54:48 +0530

    added spaces; modified ram output to make it succinct

--- MachineSummary/MachID.R
+++ MachineSummary/MachID.R
@@ -1,8 +1,8 @@
-sy<-Sys.info()
-tsstr<- format(Sys.time(), "%Y%m%d%H%M")
-cpu<-benchmarkme::get_cpu()
-ram<-benchmarkme::get_ram()
-machid<-paste(sy["nodename"],":",sy["user"],"-",sy["sysname"],"-",sy["release"],
-              "|",cpu$model_name,"|",ram,"bytesRAM", sep='')
+sy <- Sys.info()
+tsstr <- format(Sys.time(), "%Y%m%d%H%M")
+cpu <- benchmarkme::get_cpu()
+ram <- benchmarkme::get_ram()
+machid <- paste(sy["nodename"],":",sy["user"],"-",sy["sysname"],"-",sy["release"],
+              "|",cpu$model_name,"|",round(ram/1000^3,2)," GB RAM", sep='')
 cat(machid,"\n")
 
