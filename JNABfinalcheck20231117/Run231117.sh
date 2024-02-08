e4180ebdd9b48aa4857639b850f76c5bc593f3d4
John C. Nash <nashjc@uottawa.ca>
2024-02-08 10:53:23 -0800

    update finalcheck

---
+++ JNABfinalcheck20231117/Run231117.sh
@@ -0,0 +1,4 @@
+# run html processing
+Rscript -e 'rmarkdown::render("ComparingNLStools.Rmd", "rjtools::rjournal_web_article", output_options=list(), clean=FALSE)'
+Rscript -e 'rmarkdown::render("ComparingNLStools.Rmd", "rjtools::rjournal_pdf_article", output_options=list(), clean=FALSE)'
+pdflatex RJwrapper.tex
