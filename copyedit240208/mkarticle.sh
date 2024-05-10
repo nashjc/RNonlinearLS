5864cf280b8d501f32f34a3bed959e270bc08afc
John C. Nash <nashjc@uottawa.ca>
2024-05-10 00:51:47 -0400

    uncommitted

--- copyedit240208/mkarticle.sh
+++ copyedit240208/mkarticle.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 ## make article
 Rscript -e 'rmarkdown::render("2023-15-copyeditresponse.Rmd", "rjtools::rjournal_web_article", output_options=list(), clean=FALSE)'
+echo "Files:"
+ls
+read tmp
 pdflatex RJwrapper.tex
 pdflatex RJwrapper.tex
